1c4341300c97c534dfa73c80eb580edf29b420aa
sugiyama <sti320a@gmail.com>
2018-08-30 10:41:11 +0900

    0830

--- uploader_project/uploader/models.py
+++ uploader_project/uploader/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.core.validators import FileExtensionValidator
 
-# Create your models here.
+class Uploader(models.Model):
+    title = models.CharField(max_length=100)
+    attach = models.FileField(
+        upload_to='uploads/%Y/%m/%d/',
+        verbose_name='添付ファイル',
+        validators=[FileExtensionValidator(['pdf', ])],
+    )
